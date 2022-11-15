d93d143d4428a17d6f366fe6f7f6291f1aedca60
Rute Souza <rutesouza6801@gmail.com>
2022-11-14 23:34:47 -0300

    commit inicial

---
+++ src/service/PaypalService.java
@@ -0,0 +1,17 @@
+package service;
+
+public class PaypalService implements OnlinePaymentService{
+    
+    private static final double FEE_PERCENTAGE = 0.02;
+    private static final double INTEREST_PERCENTAGE = 0.01;
+
+    @Override
+    public Double interest(Double amount, Integer months) {
+        return amount * months + INTEREST_PERCENTAGE;
+    }
+    @Override
+    public Double paymentFee(Double amount) {
+        return amount * FEE_PERCENTAGE;
+    }
+
+}
